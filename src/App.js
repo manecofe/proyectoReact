057906ea661e2b4a5d7bc1352d1603cb9c8102e3
Manuel Alejandro Valderrama Fuentes <feelmaneco@gmail.com>
2021-04-19 01:31:15 -0500

    cambios

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import Card from '@material-ui/core/Card';
+import {Grid,Card,TextField,Typography} from '@material-ui/core';
+
+
 import './App.css';
 import { blue } from '@material-ui/core/colors';
 
@@ -9,9 +10,11 @@ const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
-  paper: {
+  background: {
+    background: 'linear-gradient(45deg, #49e37c 30%, #b7edc9 90%)',
     textAlign: 'center',
     color: blue,
+    spacing: '200px',
   },
 }));
 
@@ -31,7 +34,14 @@ function App() {
       >
         <Grid item xs={3} >
           <Card>
-            // card content
+            <Typography variant="h4" noWrap>
+              Welcome to
+              H2OKT
+            </Typography>
+
+
+            <TextField label="User" defaultValue="" />
+            <TextField label="Password" defaultValue="" />
           </Card>
         </Grid>      
       </Grid>
