09cc8c0a44fe90ca562b192ea26896609b60797f
Manuel Alejandro Valderrama Fuentes <feelmaneco@gmail.com>
2021-05-12 02:34:17 -0500

    arreglos

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,19 @@ import Loginform from './Loginform';
 import Singupform from './Singupform';
 
 import '../styles/App.css';
+import { helpHttp } from '../helpers/helpHttp';
 
 
 
 function App() {
+
+let url="http://localhost:5000/users";
+let api =helpHttp();  
 const users = require('../jsons/accounts2.json');
 const [user,setUser]=useState({email:"",name:"",state:""});
 const [error,setError] = useState("");
+
+
 
 const Login = details =>{
   console.log(exist(details));
@@ -41,6 +47,7 @@ const logout = () =>{
 const exist = function (details){
   var manito= false
   var nombre=""
+
   for(var i=0;i<ObjectLength(users);i++){
     if(users[i].column2==details.email && users[i].column1==details.password){
       manito=true;
@@ -59,10 +66,7 @@ const exist = function (details){
 }
 
 const testfuntion= function(){
-  var dictstring = JSON.stringify(users);
-
-  var fs = require('fs');
-  fs.writeFile("thing.json", dictstring);
+  console.log(api.get(url))
 }
 
 const ObjectLength= function ( object ) {
